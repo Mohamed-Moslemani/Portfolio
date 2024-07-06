ff6b3fa3265257ef9c3c620828354bc4efd5d70b
Mohamed Moslemani <mohamad.meselmany@hotmail.com>
2024-07-06 20:20:12 +0300

    removing the links from mid

--- index.js
+++ index.js
@@ -15,3 +15,32 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('main').style.marginLeft = '200px';
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lines = [
+        'Mohamed_Moslemani_Robotic_Version > print("Hello World")',
+        'Mohamed_Moslemani_Human_Version > print("Stop, please, Don\'t kill me")',
+        'Mohamed_Moslemani_Robotic_Version > print("Good Bye Humans, Hello world")'
+    ];
+
+    let currentLine = 0;
+    let currentChar = 0;
+    const typingSpeed = 50; // typing speed in milliseconds
+
+    function typeLine() {
+        if (currentLine < lines.length) {
+            let lineElement = document.getElementById(`line${currentLine + 1}`);
+            if (currentChar < lines[currentLine].length) {
+                lineElement.innerHTML += lines[currentLine][currentChar];
+                currentChar++;
+                setTimeout(typeLine, typingSpeed);
+            } else {
+                currentLine++;
+                currentChar = 0;
+                setTimeout(typeLine, typingSpeed);
+            }
+        }
+    }
+
+    typeLine();
+});
