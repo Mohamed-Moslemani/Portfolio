1da9cd4484a876898eb7187152249d6220a15c7d
Mohamed Moslemani <mohamad.meselmany@hotmail.com>
2024-07-06 22:39:53 +0300

    using typed.js to create typing animation

--- index.js
+++ index.js
@@ -14,5 +14,30 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.style.width = '200px';
         document.querySelector('main').style.marginLeft = '200px';
     });
-});
 
+    const lines = [
+        "Mohamed_Moslemani_Robotic_Version > print(\"Hello World\")",
+        "Mohamed_Moslemani_Human_Version > print(\"Stop, please, Don't kill me\")",
+        "Mohamed_Moslemani_Robotic_Version > print(\"Good Bye Humans, Hello world\")"
+    ];
+
+    new Typed('#line1', {
+        strings: [lines[0]],
+        typeSpeed: 50,
+        showCursor: false,
+        onComplete: () => {
+            new Typed('#line2', {
+                strings: [lines[1]],
+                typeSpeed: 50,
+                showCursor: false,
+                onComplete: () => {
+                    new Typed('#line3', {
+                        strings: [lines[2]],
+                        typeSpeed: 50,
+                        showCursor: true
+                    });
+                }
+            });
+        }
+    });
+});
