047daa6ef1ceac07a551708ab495b28b1fb92a26
Mohamed Moslemani <moslemanomohamed@gmail.com>
2024-07-16 14:06:47 +0300

    fixing the navbar effect

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const nav = document.querySelector('nav');
     nav.addEventListener('mouseover', () => {
-        nav.style.width = '210px';
-        document.querySelector('main').style.marginLeft = '100px';
+        nav.style.width = '150px';
+        document.querySelector('main').style.color = rgba(0, 0, 0, 0.5);
     });
     nav.addEventListener('mouseout', () => {
         nav.style.width = '200px';
-        document.querySelector('main').style.marginLeft = '0px';
+        document.querySelector('main').style.marginLeft = "200px";
     });
 
     const lines = [
